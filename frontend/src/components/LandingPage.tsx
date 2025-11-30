6f9f16d2be26bf4ac5f940e66c685140864eeeed
DavJesse <jesseomolo@gmail.com>
2025-11-30 15:46:32 +0300

    fix: update component to update page title (#1)

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Hero from './Hero';
 import MostPopular from './MostPopular';
 
 const LandingPage: React.FC = () => {
+  // Use useEffect to run code after the component renders
+  useEffect(() => {
+    // Set the title property
+    document.title = 'Shop';
+  }, []); 
+
   return (
     <div className="min-h-screen w-full bg-[#050505] text-white selection:bg-[#CFFF24] selection:text-black">
       <main className="flex flex-col">
